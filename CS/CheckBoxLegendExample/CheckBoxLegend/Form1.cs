ab3e658bdb3b6c38dfd969598344cc63bdf5cde5
BrianDX <boris.zaitsev@devexpress.com>
2018-09-07 17:04:57 +0300

    example content

---
+++ CS/CheckBoxLegendExample/CheckBoxLegend/Form1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CheckBoxLegend
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            ChartLegendHelper helper = new CheckBoxLegend.ChartLegendHelper();
+            helper.Attach(dashboardViewer1, "chartDashboardItem1");
+            dashboardViewer1.LoadDashboard("nwindDashboard.xml");
+        }
+    }
+}
