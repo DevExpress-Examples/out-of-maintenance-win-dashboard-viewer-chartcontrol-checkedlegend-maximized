b88fd846ab8ebe5ad482903e21a6f4632df564e7
DevExpressExampleBot <fakeEmail>
2020-12-18 15:57:17 +0300

    autogenerate VB

--- VB/CheckBoxLegendExample/CheckBoxLegend/Form1.vb
+++ VB/CheckBoxLegendExample/CheckBoxLegend/Form1.vb
@@ -8,14 +8,14 @@ Imports System.Text
 Imports System.Windows.Forms
 
 Namespace CheckBoxLegend
-    Partial Public Class Form1
-        Inherits Form
+	Partial Public Class Form1
+		Inherits Form
 
-        Public Sub New()
-            InitializeComponent()
-            Dim helper As ChartLegendHelper = New CheckBoxLegend.ChartLegendHelper()
-            helper.Attach(dashboardViewer1, "chartDashboardItem1")
-            dashboardViewer1.LoadDashboard("nwindDashboard.xml")
-        End Sub
-    End Class
+		Public Sub New()
+			InitializeComponent()
+			Dim helper As ChartLegendHelper = New CheckBoxLegend.ChartLegendHelper()
+			helper.Attach(dashboardViewer1, "chartDashboardItem1")
+			dashboardViewer1.LoadDashboard("nwindDashboard.xml")
+		End Sub
+	End Class
 End Namespace
